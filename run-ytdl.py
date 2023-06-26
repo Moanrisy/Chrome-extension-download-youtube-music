91d2a7d48bb3cb039d9a149c2b7b0ed1b67c96ad
Moanrisy <moanrisy@gmail.com>
2023-06-26 21:34:40 +0700

    Fix for linux

--- run-ytdl.py
+++ run-ytdl.py
@@ -1,66 +1,59 @@
+#!/usr/bin/env python
+
 import struct
 import sys
-import os
 import json
+import os
 import subprocess
 
-# On Windows, the default I/O mode is O_TEXT. Set this to O_BINARY
-# to avoid unwanted modifications of the input/output streams.
-if sys.platform == "win32":
-  import msvcrt
-  msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
-  msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+# Open a file to write the log
+log_file = open('native_host.log', 'w')
 
-# Thread that reads messages from the webapp.
-def read_thread_func():
-  while True:
-    # Read the message length (first 4 bytes).
-    text_length_bytes = sys.stdin.read(4)
-         
-    if not text_length_bytes:
-      sys.exit(0)
+def read_message():
+    raw_length = sys.stdin.buffer.read(4)
+    if len(raw_length) == 0:
+        sys.exit(0)
+    message_length = struct.unpack('@I', raw_length)[0]
+    message = sys.stdin.buffer.read(message_length).decode('utf-8')
+
+    json_object = json.loads(message)
+    url = json_object['url']
+    directory = '/mnt/storage/Music/unrenamed'
+
+    command = 'yt-dlp --no-playlist -o "%(artist)s - %(title)s.%(ext)s" -f 140 ' + url
+    konsole_command = f'konsole --workdir {directory} -e \'{command}\''
+    subprocess.run(konsole_command, shell=True)
+
+    # git_bash_command = 'cd "{0}" && {1}'.format(directory, command)
+    # subprocess.Popen(['bash', '-c', git_bash_command])
+
+    log_file.write(konsole_command)
+
+    return json.loads(message)
+
+def write_message(message_content):
+    encoded_message = json.dumps(message_content).encode('utf-8')
+    sys.stdout.buffer.write(struct.pack('@I', len(encoded_message)))
+    sys.stdout.buffer.write(encoded_message)
+    sys.stdout.buffer.flush()
+
+while True:
+    received_message = read_message()
     
-    # Unpack message length as 4 byte integer.
-    text_length = struct.unpack('=I', text_length_bytes)[0]
-    # Read the text (JSON object) of the message.
-    text = sys.stdin.read(text_length).encode("utf-8")
+    # Write the received message to the log file
+    # log_file.write("Received message: {}\n".format(received_message))
+    
+    # Example: Reverse the received message
+    reversed_message = received_message[::-1]
+    
+    response_message = {
+        'result': reversed_message
+    }
+    
+    # Write the response to the log file
+    log_file.write("Sending response: {}\n".format(response_message))
+    
+    write_message(response_message)
 
-    json_object = json.loads(text)
-
-    if (os.path.exists('./data')):
-      with open('./data', 'wb') as f:
-        f.write(json_object['url'])
-        f.close()
-
-        url = json_object['url']
-        directory = '/c/users/moanr/music/unrenamed'
-        command = 'yt-dlp.exe -o "%(artist)s - %(title)s.%(ext)s" -f 140 ' + url
-        
-        # Construct the command to be executed by Git Bash
-        # this work but git bash didn't auto close when complete
-        ## git_bash_command = 'cd "{0}" && {1}; bash'.format(directory, command)
-        git_bash_command = 'cd "{0}" && start /WAIT {1} && exit'.format(directory, command)
-        
-        # Launch Git Bash with the command
-        subprocess.Popen(['git-bash.exe', '-c', git_bash_command], shell=True)
-
-    else:
-      with open('./data', 'xb') as f:
-        f.write(text)
-        f.close()
-
-# Helper function that sends a message to the webapp.
-def send_message(message):
-  # Write message size.
-  sys.stdout.write(struct.pack('=I', len(message.encode("utf-8"))))
-  
-  # Write the message itself.
-  sys.stdout.write(struct.pack(str(len(message.encode("utf-8")))+"s", message.encode("utf-8")))
-  sys.stdout.flush()
-
-def index():
-  # send_message('{"msg": "Hello World!"}')
-  read_thread_func()
-
-if __name__ == '__main__':
-  index()
+# Close the log file before exiting
+log_file.close()
